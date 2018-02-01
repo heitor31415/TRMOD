3ad1fb02e0738c862af63de352a0086d6c887f31
heitor31415 <heitor31415@gmail.com>
2018-02-01 10:35:26 +0100

    Just one more phrase

---
+++ TRMOD/main1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+	printf("WORKED\n");
+	printf("Next step will be implement GSL lib (commit test)");
+	system("PAUSE");
+	return 0;
+}
